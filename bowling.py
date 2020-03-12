14232ce3f4b2d028fdc4526bf0abedc9f97502cd
Nydragon00 <6582485n@gmail.com>
2020-03-12 11:44:48 -0300

    test max/min of hit pins

--- bowling.py
+++ bowling.py
@@ -1,6 +1,11 @@
+import random as rndm
+
 class Pin_counter():
     def __init__(self):
+        self.min_range = 0
+        self.max_range = 10
         return
 
-    def chance_pin_hit(self):
-        return True
+    def range_pin_hit(self):
+        pin_gen_range = rndm.randint(self.min_range, self.max_range)
+        return pin_gen_range
--- bowling_test.py
+++ bowling_test.py
@@ -1,8 +1,17 @@
 import pytest
 import bowling
 
-class Tests():
+pin_count = bowling.Pin_counter()
 
-    def test_created_chance_pin(self):
-        pin_count = bowling.Pin_counter()
-        assert pin_count.chance_pin_hit() == True
+class Tests():
+    def test_range_int_pin(self):
+        assert isinstance(pin_count.range_pin_hit(), int) ==  True
+
+    def test_min_range_pin(self):
+        assert pin_count.min_range == 0
+        return
+
+    def test_max_range_pin(self):
+        assert pin_count.max_range == 10
+        return
+#isinstance(pin_count.chance_pin_hit(), int) ==  True
