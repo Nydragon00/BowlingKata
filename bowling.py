a44356e7ba23b6c5dcf39c419e79a765b392a707
Nydragon00 <6582485n@gmail.com>
2020-03-12 11:57:55 -0300

    test counting points

--- bowling.py
+++ bowling.py
@@ -4,7 +4,16 @@ class Pin_counter():
     def __init__(self):
         self.min_range = 0
         self.max_range = 10
+        self.turn_amount = 10
+        self.roll_per_turn = 2
+        self.overall_rolls = self.turn_amount * self.roll_per_turn
         return
+
+    def count_points(self):
+        pin_points = 0
+        for x in range(self.roll_per_turn):
+            pin_points += self.range_pin_hit()
+        return pin_points
 
     def range_pin_hit(self):
         pin_gen_range = rndm.randint(self.min_range, self.max_range)
--- bowling_test.py
+++ bowling_test.py
@@ -3,7 +3,7 @@ import bowling
 
 pin_count = bowling.Pin_counter()
 
-class Tests():
+class Tests_range_pin_hit():
     def test_range_int_pin(self):
         assert isinstance(pin_count.range_pin_hit(), int) ==  True
 
@@ -14,4 +14,9 @@ class Tests():
     def test_max_range_pin(self):
         assert pin_count.max_range == 10
         return
+
+    def test_points_per_turn(self):
+        assert  0 <= pin_count.count_points() <= 20 
+        return
+
 #isinstance(pin_count.chance_pin_hit(), int) ==  True
