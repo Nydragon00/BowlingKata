034af44b3cbaa5845c8979f6c1a09ac2650bb005
Nydragon00 <6582485n@gmail.com>
2020-03-12 13:37:31 -0300

    test counting points based on former strikes  and  spares

--- bowling.py
+++ bowling.py
@@ -8,12 +8,15 @@ class Pin_counter():
         self.roll_per_turn = 2
         self.overall_rolls = self.turn_amount * self.roll_per_turn
         self.game_score = []
+        self.game_points = []
+        self.game_length = 0
         return
 
     def count_points(self):
-        #self.count_round_points()
-        #pin_points = 0
-        return #pin_points
+        for x in range(self.turn_amount):
+            self.game_points += self.count_round_points()
+            self.game_points = self.spare_strike_checker()
+        return self.game_points
 
     def count_round_points(self):
         pin_points_round = []
@@ -30,3 +33,26 @@ class Pin_counter():
     def range_pin_hit(self):
         pin_gamount_gen = rndm.randint(self.min_range, self.max_range)
         return pin_gamount_gen
+
+    def spare_strike_checker(self):
+        self.game_length = len(self.game_points)
+        except_str = "Out of index range: first(spare)/second(strike)."
+        try:
+            current_round_points = self.game_points[self.game_length][0] + self.game_points[self.game_length][1]
+            former_round_points = self.game_points[self.game_length-1][0] + self.game_points[self.game_length-1][1]
+        except:
+            print(except_str)
+            pass
+        try:
+            if (self.game_points[self.game_length-2][0] == 10):
+                self.game_points[self.game_length-2][0] += current_round_points
+        except:
+            print(except_str)
+            pass
+        try:
+            if (former_round_points == 10):
+                self.game_points[self.game_length-1][1] += current_round_points
+        except:
+            print(except_str)
+            pass
+        return self.game_points
--- bowling_test.py
+++ bowling_test.py
@@ -15,8 +15,17 @@ class Tests_range_pin_hit():
         assert pin_count.max_range == 10
         return
 
-    def test_points_per_turn(self):
+    def test_points_is_list(self):
         assert isinstance(pin_count.count_round_points(), list) == True
         return
 
-#isinstance(pin_count.chance_pin_hit(), int) ==  True
+    def test_points_per_turn_under_10(self):
+        turn_points_list = pin_count.count_round_points()
+        combined_points = turn_points_list[0] + turn_points_list[1]
+        assert 0 <= combined_points <= 10
+        return
+
+    def test_current_point_list(self):
+        spare_strike_checked = pin_count.spare_strike_checker()
+        assert pin_count.game_length == len(pin_count.spare_strike_checker())
+        return
